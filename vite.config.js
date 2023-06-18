01a1ec52455bbc3ef56348b24013e004e617f1e6
guiyee <guillermodinanno@gmail.com>
2023-06-18 12:01:40 -0300

    Intento de deploy gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import macrosPlugin from 'vite-plugin-babel-macros'
 
 export default defineConfig({
 plugins: [react(), macrosPlugin()],
-base: "/Pre-Entrega-Guillermo-Di-Nanno-Comision-43240/"
+base: "/<Pre-Entrega-Guillermo-Di-Nanno-Comision-43240>/"
 })
