107e646f9a40afb10cd4b1311bc8427001da9ae6
guiyee <guillermodinanno@gmail.com>
2023-07-01 15:49:55 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import macrosPlugin from 'vite-plugin-babel-macros'
 
 export default defineConfig({
-plugins: [react(), macrosPlugin()],
-base:'https://guiyee89.github.io/Pre-Entrega-Guillermo-Di-Nanno-Comision-43240/'
+    base:'https://guiyee89.github.io/Pre-Entrega-Guillermo-Di-Nanno-Comision-43240/',
+    plugins: [react(), macrosPlugin()]
 })
