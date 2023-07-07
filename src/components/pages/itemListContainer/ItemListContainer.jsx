cfddb168d41feefcc838a75ac091a44773297f0b
guiyee <guillermodinanno@gmail.com>
2023-07-07 19:46:01 -0300

    Deploy en Vercel

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -3,7 +3,6 @@ import { useParams, useNavigate } from "react-router-dom";
 import { ItemList } from "./ItemList";
 import { LoaderBar } from "../../common/Loaders/LoaderBar";
 import { CartContext } from "../../context/CartContext";
-import { ToasterContainer } from "../../common/loaders/ToasterContainer";
 import "react-toastify/dist/ReactToastify.css";
 import { db } from "../../../firebaseConfig";
 import { collection, getDocs, query, where } from "firebase/firestore";
@@ -67,7 +66,6 @@ export const ItemListContainer = () => {
   //Rendering condicional
   return (
     <div>
-      <ToasterContainer />
       {loading ? (
         <LoaderBar />
       ) : (
