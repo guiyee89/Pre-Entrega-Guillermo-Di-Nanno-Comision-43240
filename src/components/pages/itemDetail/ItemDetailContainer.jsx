49d118df43711fe4df132411c542b53daf096a85
guiyee <guillermodinanno@gmail.com>
2023-07-19 16:11:45 -0300

    asd

--- src/components/pages/itemDetail/ItemDetailContainer.jsx
+++ src/components/pages/itemDetail/ItemDetailContainer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { ItemDetail } from "./ItemDetail";
 import { CartContext } from "../../context/CartContext";
 import { db } from "../../../firebaseConfig";
@@ -10,46 +10,42 @@ import "react-toastify/dist/ReactToastify.css";
 import { BarLoader } from "react-spinners";
 
 export const ItemDetailContainer = () => {
+  //Guardamos los items (objetos)
+  const [selectedItem, setSelectedItem] = useState({});
 
-  const { id } = useParams();//react-router-dom
-  const { state } = useLocation(); //Para desestructurar la info del objeto traido con react-router-dom
-  const [selectedItem, setSelectedItem] = useState(state?.selectedItem);//Inicializamos useState de esta forma para identificar si 
-  //el state de navegacion proviene de Carousel o de ItemList (que no posee dicho state)
+  //PROVEEMOS EL "CONTEXTO"
   const { addToCart } = useContext(CartContext);
-  const [loading, setLoading] = useState(false);
 
-  //Agregando items al carrito
+  const { id } = useParams();
+
+  //Obtener cantidades por ID para pasar la data
+  // const quantityId = getTotalQuantityById(id);
+
+  //AGREGAMOS PRODUCTOS AL CARRITO
   const onAdd = (quantity) => {
     let data = {
       ...selectedItem,
       quantity: quantity,
     };
-    console.log(data)
+     //Agregamos la "data" de los productos con la funcion de contexto
     addToCart(data);
-    setSelectedItem({ ...selectedItem, quantity: 1 });
+    setSelectedItem({ ...selectedItem, quantity: 1 }); //Reset count inicial a 1
   };
 
-  //fetcheando los productos. Gracias a esta funcion, fetcheamos desde ItemList y Carousel
+  //ENCONTRAMOS PRODUCTOS POR "ID" Y RESOLVEMOS PROMISE PARA RENDERIZAR
   useEffect(() => {
-    const fetchSelectedItem = async () => {
-      setLoading(true);
-      const itemCollection = collection(db, "products");
-      const refDoc = doc(itemCollection, id);
-      const docSnapshot = await getDoc(refDoc);
-      console.log(docSnapshot)
-      if (docSnapshot.exists()) {
+    let itemCollection = collection(db, "products");
+    let refDoc = doc(itemCollection, id);
+
+    setTimeout(() => {
+      getDoc(refDoc).then((response) => {
         setSelectedItem({
-          ...docSnapshot.data(),
-          id: docSnapshot.id,
+          ...response.data(),
+          id: response.id,
         });
-        
-      }
-      setLoading(false);
-    };
-
-    fetchSelectedItem();
+      });
+    }, 800);
   }, [id]);
-
 
   return (
     <>
@@ -65,23 +61,20 @@ export const ItemDetailContainer = () => {
         pauseOnHover
         theme="dark"
       />
-      {loading ? (
-        <LoaderWrapper>
-          <BarLoader color="#12352e" width={250} />
-        </LoaderWrapper>
+      {selectedItem.id ? (
+        <ItemDetail
+          selectedItem={selectedItem}
+          onAdd={onAdd}
+          addToCart={addToCart}
+        />
       ) : (
-        selectedItem && (
-          <ItemDetail
-            selectedItem={selectedItem}
-            onAdd={onAdd}
-            addToCart={addToCart}
-          />
-        )
+        <LoaderWrapper>
+          <BarLoader color="#12352e"  width={250}/>
+        </LoaderWrapper>
       )}
     </>
   );
 };
-
 const LoaderWrapper = styled.div`
   display: flex;
   justify-content: center;
