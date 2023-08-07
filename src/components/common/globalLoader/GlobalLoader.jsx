70a95551abc76ebe8810264ca359b3ed527f4f34
guiyee <guillermodinanno@gmail.com>
2023-08-06 23:56:14 -0300

    asd

--- src/components/common/globalLoader/GlobalLoader.jsx
+++ src/components/common/globalLoader/GlobalLoader.jsx
@@ -1,34 +1,34 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import styled from "styled-components/macro";
+// import { useEffect, useState } from "react";
+// import { useLocation } from "react-router-dom";
+// import styled from "styled-components/macro";
 
 
-export const GlobalLoader = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const location = useLocation();
+// export const GlobalLoader = ({ children }) => {
+//   const [loading, setLoading] = useState(true);
+//   const location = useLocation();
 
-  //Global "Flash" Conditional
-  useEffect(() => {
-    setLoading(true);
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 450);
+//   //Global "Flash" Conditional
+//   useEffect(() => {
+//     setLoading(true);
+//     const timer = setTimeout(() => {
+//       setLoading(false);
+//     }, 450);
 
-    return () => clearTimeout(timer);
-  }, [location]);
+//     return () => clearTimeout(timer);
+//   }, [location]);
 
 
-  return loading ? (
-    <Wrapper>
-    </Wrapper>
-  ) : (
-    children
-  );
-};
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    margin-left: 20px;
-`
+//   return loading ? (
+//     <Wrapper>
+//     </Wrapper>
+//   ) : (
+//     children
+//   );
+// };
+// const Wrapper = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     height: 100%;
+//     margin-left: 20px;
+// `
