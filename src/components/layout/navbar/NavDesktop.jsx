c4e6901b050462a11be075ecfed078bbd26631de
guiyee <guillermodinanno@gmail.com>
2023-12-27 14:19:34 -0300

    probando fixed bug login

--- src/components/layout/navbar/NavDesktop.jsx
+++ src/components/layout/navbar/NavDesktop.jsx
@@ -180,7 +180,7 @@ export const NavDesktop = () => {
                     sx={{ padding: "10px" }}
                     totalItems={totalItems}
                   />
-                  {user.rol === rolAdmin ||
+                  {/* {user.rol === rolAdmin ||
                   user.rol === rolAdmin2 ||
                   user.rol === rolAdmin3 ||
                   user.rol === "user" ? null : (
@@ -206,6 +206,37 @@ export const NavDesktop = () => {
                     </>
                   ) : null}
                   {user.rol === "user" && (
+                    <>
+                      <ProfileBtn>
+                        <h4>Profile</h4>
+                        <AccountCircleSharpIcon
+                          sx={{ fontSize: "30px", marginBottom: "-13px" }}
+                          onClick={() => navigate("/user-orders")}
+                        />
+                      </ProfileBtn>
+                    </>
+                  )} */}
+                  {!user || !user.rol ? (
+                    <LoginBtn>
+                      <h4>Login / Sign up</h4>
+                      <LoginOutlinedIcon
+                        sx={{ fontSize: "26px" }}
+                        onClick={() => navigate("/login")}
+                      />
+                    </LoginBtn>
+                  ) : user.rol === rolAdmin ||
+                    user.rol === rolAdmin2 ||
+                    user.rol === rolAdmin3 ? (
+                    <>
+                      <DashboardBtn scrolled={scroll}>
+                        <h4>Admin</h4>
+                        <DashboardCustomizeIcon
+                          sx={{ fontSize: "27px" }}
+                          onClick={() => navigate("/dashboard")}
+                        />
+                      </DashboardBtn>
+                    </>
+                  ) : (
                     <>
                       <ProfileBtn>
                         <h4>Profile</h4>
